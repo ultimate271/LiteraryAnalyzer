9706de2cd8e6d357fb9bd419e665317fc8b044cb
Brett Webster <bfireflash@gmail.com>
2018-04-03 07:59:23 -0500

    Initial Commit

---
+++ LiteraryAnalyzer/LiteraryAnalyzer/Excerpt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace LiteraryAnalyzer {
+	public class Excerpt {
+		public int ExcerptID { get; set; }
+		public string ExcerptText { get; set; }
+
+		public virtual List<Excerpt> Children { get; set; }
+	}
+	public class LiteraryAnalyzerContext : DbContext {
+		public DbSet<Excerpt> Excerpts { get; set; }
+	}
+}
---
+++ LiteraryAnalyzer/LiteraryAnalyzer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteraryAnalyzer {
+	class Program {
+		static void Main(string[] args) {
+			using (var db = new LiteraryAnalyzerContext()) {
+				foreach (Excerpt e in db.Excerpts.OrderBy(e => e.ExcerptID)) {
+					System.Console.WriteLine(e.ExcerptText);
+				}
+				System.Console.ReadLine();
+			}
+			//using (var db = new LiteraryAnalyzerContext()) {
+			//	var root = new Excerpt { ExcerptText = "Root" };
+			//	db.Excerpts.Add(root);
+			//	db.SaveChanges();
+			//}
+		}
+	}
+}
