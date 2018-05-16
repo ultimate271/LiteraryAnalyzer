d123e5257bd4922aea2784b712c7a81a93894e92
Brett Webster <bwebster@alliantsystems.corp>
2018-05-16 10:01:50 -0500

    Successful merge?

---
+++ LiteraryAnalyzer/LiteraryAnalyzer/Controller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteraryAnalyzer {
+	public class Controller {
+		private LiteraryAnalyzerContext db;
+		public Controller() {
+			db = new LiteraryAnalyzerContext();
+		}
+		public void DeveloperDebug (String markdown) {
+			String[] sentences = markdown.Split('.','?','!');
+			var lines = markdown.Replace("\r", "").Split('\n')
+				.Where(l => !l.StartsWith("#"));
+			System.Console.WriteLine(sentences.Length);
+
+			var paras = new List<String>();
+			var sb = new StringBuilder();
+			foreach (String line in lines) {
+				if (String.IsNullOrEmpty(line)) {
+					paras.Add(sb.ToString());
+					sb.Clear();
+				}
+				else { 
+					sb.Append(line).Append(" ");
+				}
+			}
+			foreach (String para in paras) {
+				System.Console.Write("{0}: ", para.Count(c => c == '.' || c == '?' || c == '!'));
+				foreach (String sen in para.Split('.', '?', '!')) {
+					System.Console.Write("{0}, ", sen.Count(c => c == ' '));
+				}
+				System.Console.WriteLine();
+			}
+			System.Console.WriteLine(paras.Count);
+		}
+		/// <summary>
+		/// Takes a markdown file and inserts records into the db for 
+		/// </summary>
+		/// <param name="markdown"></param>
+		public bool ParseMarkdown (String markdown) {
+			
+		}
+	}
+}
--- LiteraryAnalyzer/LiteraryAnalyzer/Program.cs
+++ LiteraryAnalyzer/LiteraryAnalyzer/Program.cs
@@ -7,16 +7,22 @@ using System.Threading.Tasks;
 namespace LiteraryAnalyzer {
 	class Program {
 		static void Main(string[] args) {
-			using (var db = new LiteraryAnalyzerContext()) {
-				foreach (Excerpt e in db.Excerpts.OrderBy(e => e.ExcerptID)) {
-					System.Console.WriteLine(e.ExcerptText);
-				}
-				System.Console.ReadLine();
-			}
+			var action = new Controller();
+			action.DeveloperDebug(System.IO.File.ReadAllText(@"C:\Users\bwebster\Source\Repos\notes\tolkien\sil22.md"));
+			System.Console.ReadLine();
 			//using (var db = new LiteraryAnalyzerContext()) {
-			//	var root = new Excerpt { ExcerptText = "Root" };
-			//	db.Excerpts.Add(root);
+			//	var root = db.Excerpts.Where(e => e.ExcerptID == 1).First();
+			//	//var newChild = new Excerpt { ExcerptText = "A child of root" };
+			//	//root.Children.Add(newChild);
+			//	//root.Children.Remove(root.Children.Where(e => e.ExcerptID == 3).First());
+			//	//db.Excerpts.Remove(db.Excerpts.Where(e => e.ExcerptID == 3).First());
 			//	db.SaveChanges();
+			//}
+			//using (var db = new LiteraryAnalyzerContext()) {
+			//	foreach (Excerpt e in db.Excerpts.OrderBy(e => e.ExcerptID)) {
+			//		System.Console.WriteLine(e.ExcerptText);
+			//	}
+			//	System.Console.ReadLine();
 			//}
 		}
 	}
